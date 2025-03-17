8d6296722121dff980429de102e34979c75f2fb1
mdrsqy <mdrsqy@gmail.com>
2025-03-17 22:43:32 +0700

    Initialize Kwizzle with Spring Boot

---
+++ src/main/java/com/kwizzle/KwizzleApplication.java
@@ -0,0 +1,13 @@
+package com.kwizzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwizzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwizzleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwizzle/KwizzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwizzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwizzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
