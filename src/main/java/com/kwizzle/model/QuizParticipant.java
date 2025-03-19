98c110f747581e923cbcb51b94db2c5b0ef87610
mdrsqy <mdrsqy@gmail.com>
2025-03-19 21:40:52 +0700

    update: create QuizParticipant Model

---
+++ src/main/java/com/kwizzle/model/QuizParticipant.java
@@ -0,0 +1,65 @@
+package com.kwizzle.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "quiz_participant")
+public class QuizParticipant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private Quiz quiz;
+
+    @Column(name = "joined_at")
+    private LocalDateTime joinedAt = LocalDateTime.now();
+
+    public QuizParticipant() {
+    }
+
+    public QuizParticipant(User user, Quiz quiz, LocalDateTime joinedAt) {
+        this.user = user;
+        this.quiz = quiz;
+        this.joinedAt = joinedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    public LocalDateTime getJoinedAt() {
+        return joinedAt;
+    }
+
+    public void setJoinedAt(LocalDateTime joinedAt) {
+        this.joinedAt = joinedAt;
+    }
+}
